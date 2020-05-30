747b9a7ee5b214a8fb70ca3db19de742a2b6e81b
Ajay Kapuria <jkapuriya6@gmail.com>
2020-05-30 17:23:02 +0530

    Routing,URL and LookUP

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,10 +1,25 @@
-from django.shortcuts import render
-
+from django.shortcuts import render , get_object_or_404
+from django.http import Http404
 # Create your views here.
 from .models import BlogPost
 
-def blog_post_detail_page(requests):
-    obj = BlogPost.objects.get(id=2)
+# def blog_post_detail_page(requests, id):
+#     try:
+#         obj = BlogPost.objects.get(id=id)
+#     except BlogPost.DoesNotExist:
+#         raise Http404
+#     except ValueError:
+#         raise Http404
+#     template_name = 'blog_post_detail.html'
+#     context = {"object": obj}
+#     return render(requests, template_name, context)
+
+def blog_post_detail_page(requests, slug):
+   # obj = get_object_or_404(BlogPost, slug=slug)
+    querySet = BlogPost.objects.filter(slug=slug)
+    if querySet.count() == 0:
+        raise Http404
+    obj = querySet.first()
     template_name = 'blog_post_detail.html'
     context = {"object": obj}
     return render(requests, template_name, context)
